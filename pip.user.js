dc72c9b650c79ae20ee7569df42a4026ec3a597b
Cathal Colgan <cathal-c@hotmail.com>
2025-05-05 19:05:09 +0100

    Updated userscript

--- pip.user.js
+++ pip.user.js
@@ -7,6 +7,7 @@
 // @updateURL    https://github.com/cathalbahn/UserScripts/raw/main/pip.user.js
 // @downloadURL  https://github.com/cathalbahn/UserScripts/raw/main/pip.user.js
 // @match        https://*
+// @match        https://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // ==/UserScript==
 
